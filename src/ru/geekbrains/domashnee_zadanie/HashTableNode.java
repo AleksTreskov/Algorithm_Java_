36478a38591dce1bff4b32f52980cc703ae24d33
Aw0ken0 <82842195+Aw0ken0@users.noreply.github.com>
2021-08-18 16:38:55 +0300

    Lesson_8

---
+++ src/ru/geekbrains/domashnee_zadanie/HashTableNode.java
@@ -0,0 +1,40 @@
+package ru.geekbrains.domashnee_zadanie;
+
+public class HashTableNode {  private int key;
+    private int value;
+
+    HashTableNode(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+
+        final HashTableNode other = (HashTableNode) object;
+
+        return key == other.key && value == other.value;
+    }
+}
